efa2946c63861954bcbe52aaaec3696c51bfc2e8
Onu Wisdom <onuwisdom2000@gmail.com>
2025-06-06 16:25:49 +0100

    My first debug

--- js/script.js
+++ js/script.js
@@ -2,6 +2,23 @@ const global = {
 	currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+	const results = await fetchApiData('movie/popular');
+	console.log(results);
+}
+
+// Fetch data from TMDB API
+async function fetchApiData(endpoint) {
+	const API_KEY = '78e8755edca9e4310b6148170aed5c4f';
+	const API_URL = 'https://api.themoviedb.org/3/';
+
+	const res = await fetch(
+		`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+	);
+	const data = await res.json();
+	return data;
+}
+
 // Highlight Active Links
 function highlightActiveLinks() {
 	const links = document.querySelectorAll('.nav-link');
@@ -20,7 +37,7 @@ function Init() {
 		case '/':
 		case '/index.html':
 			// Home page initialization
-			console.log('Initializing Home Page');
+			displayPopularMovies();
 			break;
 		case '/shows.html':
 			console.log('Shows Page');
